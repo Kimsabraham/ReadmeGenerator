ab7dc9e91e44169160e7ad73a9ddaf0624d6ef7e
Abraham Kim <krnpride730@yahoo.com>
2021-04-11 17:45:20 -0400

    added packages needed and first question

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,21 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const util = require('util')
+
+const AsyncFile = util.promisify(fs.watchFile)
 
 // TODO: Create an array of questions for user input
+function AskUser (){
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the project?',
+            name: 'Title'
+
+        }
+    ])
+}
 const questions = [];
 
 // TODO: Create a function to write README file
