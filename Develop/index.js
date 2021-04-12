37bf7c7708752a2249e29ea98744ff3f59b969cb
Abraham Kim <krnpride730@yahoo.com>
2021-04-12 12:53:50 -0400

    added all questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,20 +1,51 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
-const fs = require('fs')
-const util = require('util')
+const inquirer = require("inquirer");
+const fs = require("fs"); 
+const generatorMarkdown = require("./generateMarkdown");
 
 const AsyncFile = util.promisify(fs.watchFile)
 
 // TODO: Create an array of questions for user input
 function AskUser (){
     return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the name of the project?',
-            name: 'Title'
+      {
+        type: "input",
+        message: "What is the name of the project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "Enter a description of your project.",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "What technologies need to be installed for your project?",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "What is this App going to be used for?",
+        name: "usage",
+      },
 
-        }
-    ])
+      {
+        type: "checkbox",
+        message: "Select a license for this project.",
+        choices: ["Apache", "GNU GPLv3", "ISC", "MIT"],
+        name: "license",
+      },
+      {
+        type: "input",
+        message: "Please list all contributors for this project.",
+        name: "contributors",
+      },
+      {
+        type: "input",
+        message: "How do you test your app?",
+        name: "test",
+      },
+    ]);
 }
 const questions = [];
 
